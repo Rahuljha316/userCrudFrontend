b3fef84c10fba41209507eeb8bf1ad3b1f88bca0
Rahul Jha <rahulbudy11@gmail.com>
2025-01-05 00:02:26 +0530

    front end project initialized

---
+++ src/app/users/page.js
@@ -0,0 +1,11 @@
+import UserList from "@/components/user-list"
+
+const UserPage = () => {
+    return (
+        <div>
+            <UserList />
+        </div>
+    )
+}
+
+export default UserPage
